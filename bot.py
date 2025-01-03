a113510966ccde5a55504857a34c1ce6193a99c5
godbard09 <141386814+godbard09@users.noreply.github.com>
2025-01-03 14:37:02 +0700

    bot.py

--- bot.py
+++ bot.py
@@ -28,9 +28,9 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         "Dưới đây là các lệnh bạn có thể sử dụng:\n"
         "Gõ /chart <mã giao dịch> để xem biểu đồ kỹ thuật (ví dụ: /chart BTC/USDT).\n"
         "Gõ /top để xem top 10 cặp giao dịch tăng, giảm mạnh nhất 24 giờ qua.\n"
-        "Gõ /signal <mã giao dịch> để nhận tín hiệu mua bán và lưu lịch sử.\n"
-        "Gõ /smarttrade <mã giao dịch> để xem khuyến nghị tự động.\n"
-        "Gõ /list để xem top 10 cặp giao dịch có tín hiệu mua và bán gần đây."
+        "Gõ /signal <mã giao dịch> để xem lịch sử tín hiệu mua bán trong 7 ngày qua.\n"
+        "Gõ /smarttrade <mã giao dịch> để xem thông tin và tín hiệu mua bán mới nhát.\n"
+        "Gõ /list để xem top 10 cặp giao dịch có tín hiệu mua bán gần đây."
     )
 
 
